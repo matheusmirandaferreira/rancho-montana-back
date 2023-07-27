ab6ef794af3bcbddf34f8f588425b46d78720825
Matheus de Miranda Ferreira <matheusdemirandaferreira@gmail.com>
2023-07-27 20:12:40 -0300

    API routes pattern

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,44 +15,36 @@ routes.use('/storage/public', express.static('storage/public'));
 
 routes.post('/api/auth', new UserController().login);
 
-routes.post(
-  '/api/user/create',
-  authMiddleware,
-  new UserController().createUser
-);
+routes.post('/api/user', authMiddleware, new UserController().createUser);
 
-routes.put(
-  '/api/user/:uuid/edit',
-  authMiddleware,
-  new UserController().editUser
-);
+routes.put('/api/user/:uuid', authMiddleware, new UserController().editUser);
 
 routes.get('/api/user', authMiddleware, new UserController().getUsers);
 routes.get('/api/user/:uuid', authMiddleware, new UserController().getUser);
 
 routes.get('/api/pace', authMiddleware, new PaceController().list);
 routes.get('/api/pace/:uuid', authMiddleware, new PaceController().getPace);
-routes.post('/api/pace/create', authMiddleware, new PaceController().create);
-routes.put('/api/pace/:uuid/edit', authMiddleware, new PaceController().edit);
+routes.post('/api/pace', authMiddleware, new PaceController().create);
+routes.put('/api/pace/:uuid', authMiddleware, new PaceController().edit);
 routes.delete('/api/pace/:uuid', authMiddleware, new PaceController().delete);
 
 routes.get('/api/color', authMiddleware, new ColorController().list);
 routes.get('/api/color/:uuid', authMiddleware, new ColorController().getColor);
-routes.post('/api/color/create', authMiddleware, new ColorController().create);
-routes.put('/api/color/:uuid/edit', authMiddleware, new ColorController().edit);
+routes.post('/api/color', authMiddleware, new ColorController().create);
+routes.put('/api/color/:uuid', authMiddleware, new ColorController().edit);
 routes.delete('/api/color/:uuid', authMiddleware, new ColorController().delete);
 
 routes.get('/api/race', authMiddleware, new RaceController().list);
 routes.get('/api/race/:uuid', authMiddleware, new RaceController().getRace);
-routes.post('/api/race/create', authMiddleware, new RaceController().create);
-routes.put('/api/race/:uuid/edit', authMiddleware, new RaceController().edit);
+routes.post('/api/race', authMiddleware, new RaceController().create);
+routes.put('/api/race/:uuid', authMiddleware, new RaceController().edit);
 routes.delete('/api/race/:uuid', authMiddleware, new RaceController().delete);
 
 routes.get('/api/horse', new HorseController().list);
 routes.get('/api/horse/:uuid', new HorseController().getHorse);
 routes.post('/api/horse/:uuid/image', new HorseController().uploadImage);
-routes.post('/api/horse/create', authMiddleware, new HorseController().create);
-routes.put('/api/horse/:uuid/edit', authMiddleware, new HorseController().edit);
+routes.post('/api/horse', authMiddleware, new HorseController().create);
+routes.put('/api/horse/:uuid', authMiddleware, new HorseController().edit);
 routes.delete('/api/horse/:uuid', authMiddleware, new HorseController().delete);
 
 routes.get('/api/category', authMiddleware, new CategoryController().list);
